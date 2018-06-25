d86081ce4a9ff32c16fd92558aa3c3f3dd39fdf8
kelcol <colhk@seattleu.edu>
2018-06-24 18:20:45 -0700

    removed unnecessary ui and data stuff

--- app.js
+++ app.js
@@ -1,213 +1,152 @@
-// Storage Controller
-
 // Tweet Controller
 const TweetCtrl = (function () {
-  // Tweet Constructor
-  const Tweet = function (id, creator, work, url, statement) {
-    this.id = id;
-    this.creator = creator;
-    this.work = work;
-    this.url = url;
-    this.statement = statement;
-    // this.thisOne = thisifyOne(this.statement);
-  }
+    const Tweet = function (creator, work, url, statement) {
+        this.creator = creator;
+        this.work = work;
+        this.url = url;
+        this.statement = statement;
+    };
 
-  // Data Structure / State
-  const data = {
-    tweets: [],
-    currentTweet: null,
-    totalWork: 0
-  }
+    // Public Methods
+    return {
+        makeTweet: function (creator, work, url, statement) {
 
-  // Public Methods
-  return {
-    getTweets: function () {
-      return data.tweets;
-    },
-    addTweet: function (creator, work, url, statement) {
-      let ID;
-      // Create id
-      if (data.tweets.length > 0) {
-        ID = data.tweets[data.tweets.length - 1].id + 1;
-      } else {
-        ID = 0;
-      }
-      statement = thisify();
+            statement = thisify();
 
-      // Create hashtags      
-      function thisify() {
-        let str1 = statement;
-        let str2 = statement;
-        let acronym, nextWord;
+            // Create hashtags
+            function thisify() {
+                let str1 = statement;
+                let str2 = statement;
+                let acronym, nextWord;
 
-        str1 = str1.toLowerCase().split(' ');
-        for (var i = 0; i < str1.length; i++) {
-          str1[i] = str1[i].charAt(0).toUpperCase() + str1[i].slice(1);
+                str1 = str1.toLowerCase().split(" ");
+                for (var i = 0; i < str1.length; i++) {
+                    str1[i] = str1[i].charAt(0).toUpperCase() + str1[i].slice(1);
+                }
+
+                words = str2.split(" ");
+                acronym = "";
+                index = 0;
+                while (index < words.length) {
+                    nextWord = words[index];
+                    acronym = acronym + nextWord.charAt(0);
+                    index = index + 1;
+                }
+
+                return str1.join("") + "," + acronym.toUpperCase();
+            }
+
+            // Create new tweet
+            newTweet = new Tweet(creator, work, url, statement);
+
+            return newTweet;
         }
 
-        words = str2.split(' ');
-        acronym = "";
-        index = 0
-        while (index < words.length) {
-          nextWord = words[index];
-          acronym = acronym + nextWord.charAt(0);
-          index = index + 1;
-        }
-        
-        return '#' + str1.join('') + ' ' + '#' + acronym.toUpperCase();
-
-      };
-
-      // thisTwo = this.thisifyTwo(statement)
-
-      // Create new tweet
-      newTweet = new Tweet(ID, creator, work, url, statement);
-
-      // Add to tweets array
-      data.tweets.push(newTweet);
-
-      return newTweet;
-    },
-    logData: function () {
-      return data;
     }
-  }
+
 })();
+
 
 // UI Controller
 const UICtrl = (function () {
-  const UISelectors = {
-    tweetList: '#tweet-list',
-    addBtn: '.add-btn',
-    tweetCreatorInput: '#creator',
-    tweetWorkInput: '#work',
-    tweetURLInput: '#url',
-    tweetStatementInput: '#statement'
-  }
+    const UISelectors = {
+        tweetBtn: "#twt-btn",
+        tweetCreatorInput: "#creator",
+        tweetWorkInput: "#work",
+        tweetURLInput: "#url",
+        tweetStatementInput: "#statement"
+    };
 
-  // Public Methods
-  return {
-    populateTweetList: function (tweets) {
-      let html = '';
-      tweets.forEach(function (tweet) {
-        html += `        
-        <div class="collection-item">
+    // Public Methods
+    return {
+        // Get input
+        getTweetInput: function () {
+            return {
+                creator: document.querySelector(UISelectors.tweetCreatorInput).value,
+                work: document.querySelector(UISelectors.tweetWorkInput).value,
+                url: document.querySelector(UISelectors.tweetURLInput).value,
+                statement: document.querySelector(UISelectors.tweetStatementInput).value
+            }
+        },
 
-        </div>
-        `;
-      });
+        // create Tweet link
+        createTweetLink: function () {
+            let textParam = `${newTweet.creator} - ${newTweet.work}`;
+            let urlParam = newTweet.url;
+            let hashtagsParam = newTweet.statement;
+            let tweetURL = `https://twitter.com/intent/tweet?text=${textParam}&hashtags=${hashtagsParam}&url=${urlParam}`;
 
-      // Insert list tweets
-      document.querySelector(UISelectors.tweetList).innerHTML = html;
-    },
-    getTweetInput: function () {
-      return {
-        creator: document.querySelector(UISelectors.tweetCreatorInput).value,
-        work: document.querySelector(UISelectors.tweetWorkInput).value,
-        url: document.querySelector(UISelectors.tweetURLInput).value,
-        statement: document.querySelector(UISelectors.tweetStatementInput).value,
-      }
-    },
-    addListTweet: function (tweet) {
+            // TODO: Add to href attr of share button
+            console.log(tweetURL)
+        },
 
-      // Show list
-      document.querySelector(UISelectors.tweetList).style.display = 'block';
 
-      // Create div element
-      const div = document.createElement('div');
-      // Add class
-      div.classCreator = 'collection-tweet card';
+        // Clear input
+        clearInput: function () {
+            document.querySelector(UISelectors.tweetCreatorInput).value = "";
+            document.querySelector(UISelectors.tweetWorkInput).value = "";
+            document.querySelector(UISelectors.tweetURLInput).value = "";
+            document.querySelector(UISelectors.tweetStatementInput).value = "";
+        },
+        getSelectors: function () {
+            return UISelectors;
+        }
 
-      let text = `${tweet.creator} - ${tweet.work}`
-      let hashtags = tweet.statement.replace(/#/g,'').replace(/ /g,',');
-
-      // TODO: Figure out line breaks
-      // TODO: Maybe append tweet button in separate template
-      // TODO: Encode whole uri?
-
-      // Add html
-      div.innerHTML = `
-      <div class="card-body">
-      ${tweet.creator} - ${tweet.work}
-      <br><br>
-      ${tweet.statement}
-      <br>
-      ${tweet.url}      
-      <a href="https://twitter.com/intent/tweet?text=${text}&hashtags=${hashtags}"  class="twitter-share-button" data-size="large">Tweet</a>
-      </div>
-      `;
-      
-      console.log(hashtags);
-      console.log();
-
-      // Insert tweet
-      document.querySelector(UISelectors.tweetList).insertAdjacentElement('beforeend', div)
-    },
-    clearInput: function () {
-      document.querySelector(UISelectors.tweetCreatorInput).value = '';
-      document.querySelector(UISelectors.tweetWorkInput).value = '';
-      document.querySelector(UISelectors.tweetURLInput).value = '';
-      document.querySelector(UISelectors.tweetStatementInput).value = '';
-    },
-    hideList() {
-      document.querySelector(UISelectors.tweetList).style.display = 'none';
-    },
-    getSelectors: function () {
-      return UISelectors;
     }
-  }
+
 })();
 
 // App Controller
 const App = (function (TweetCtrl, UICtrl) {
-  // Load event listeners
-  const loadEventListeners = function () {
-    // Get UI Selectors
-    const UISelectors = UICtrl.getSelectors();
+    // Load event listeners
+    const loadEventListeners = function () {
 
-    // Add tweet event
-    document.querySelector(UISelectors.addBtn).addEventListener('click', tweetAddSubmit);
-  }
+        // Get UI Selectors
+        const UISelectors = UICtrl.getSelectors();
 
-  // Add tweet submit
-  const tweetAddSubmit = function (e) {
-    // Get form input from UI Controller
-    const input = UICtrl.getTweetInput();
+        // Send tweet event
+        document
+            .querySelector(UISelectors.tweetBtn)
+            .addEventListener("click", submitTweet);
+    };
 
-    // Check for inputs
-    if (input.creator !== '' && input.work !== '' && input.url !== '' && input.statement !== '') {
+    // Submit Tweet
+    const submitTweet = function (e) {
 
-      // Add tweet
-      const newTweet = TweetCtrl.addTweet(input.creator, input.work, input.url, input.statement);
+        // Get form input from UI Controller
+        const input = UICtrl.getTweetInput();
 
-      // Add tweet to UI list
-      UICtrl.addListTweet(newTweet);
+        // Check for inputs
+        // TODO: Add better validation with messaging
+        if (
+            input.creator !== "" &&
+            input.work !== "" &&
+            input.url !== "" &&
+            input.statement !== ""
+        ) {
+            // Add tweet
+            const newTweet = TweetCtrl.makeTweet(
+                input.creator,
+                input.work,
+                input.url,
+                input.statement
+            );
 
-      // Clear fields
-      UICtrl.clearInput();
-    }
-    e.preventDefault();
-  }
+            // Tweet link
+            UICtrl.createTweetLink(newTweet);
 
-  // Public methods
-  return {
-    init: function () {
-      // Fetch tweets from data structure
-      const tweets = TweetCtrl.getTweets();
+            // Clear fields
+            UICtrl.clearInput();
+        }
+        e.preventDefault();
+    };
 
-      // check if any tweets
-      if (tweets.length === 0) {
-        UICtrl.hideList();
-      } else {
-        // Populate list with tweets
-        UICtrl.populateTweetList(tweets);
-      }
-
-      // Load event listeners
-      loadEventListeners();
-    }
-  }
-
+    // Public methods
+    return {
+        init: function () {
+            loadEventListeners();
+        }
+    };
 })(TweetCtrl, UICtrl);
 
 // Initilize App
